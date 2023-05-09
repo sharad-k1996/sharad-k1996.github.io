2989c607bda9340ad751ef449179dea6776a2431
sharad-k1996 <111413061+sharad-k1996@users.noreply.github.com>
2023-05-09 20:16:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -118,7 +118,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "#b61924",
+      "background_color": "black",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
